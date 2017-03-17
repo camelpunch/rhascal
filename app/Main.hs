c5b5001e80015411d2de85358bf0adb2996a8e6d
Andrew Bruce <me@andrewbruce.net>
2017-03-17 22:53:21 +0000

    whitespace

--- app/Main.hs
+++ app/Main.hs
@@ -20,38 +20,40 @@ main = do
 
 loop :: Game -> IO ()
 loop (Game []) = do
-    g <- getStdGen
-    clearScreen
-    let board = generateBoard g width height
-        game = Game [board]
-    setCursorPosition 0 0
-    print board
-    printTurns [board]
-    loop game
+  g <- getStdGen
+  clearScreen
+  let board = generateBoard g width height
+      game = Game [board]
+  setCursorPosition 0 0
+  print board
+  printTurns [board]
+  loop game
   where
     width = 80
     height = 20
 loop (Game turns@(previousTurn:_)) =
-    getChar >>= maybe ignore process . requestFromKey
+  getChar >>= maybe ignore process . requestFromKey
   where
     ignore = loop $ Game turns
     process request = do
-        setCursorPosition 0 0
-        printChanges $ changedRows previousTurn newTurn
-        printTurns (newTurn : turns)
-        loop $ Game (newTurn : turns)
+      setCursorPosition 0 0
+      printChanges $ changedRows previousTurn newTurn
+      printTurns (newTurn : turns)
+      loop $ Game (newTurn : turns)
       where
         newTurn = nextTurn request previousTurn
-    printChanges = traverse_ $ maybe skipRow printRow
+
+printChanges :: [Maybe [Tile]] -> IO ()
+printChanges = traverse_ $ maybe skipRow printRow
+  where
     skipRow = cursorDownLine 1
     printRow = putStrLn . showRow
 
 printTurns :: [Board] -> IO ()
 printTurns turns = do
-    setCursorPosition height 0
-    putStrLn $ "Turn: " ++ show (length turns)
+  setCursorPosition height 0
+  putStrLn $ "Turn: " ++ show (length turns)
   where
-    height =
-        case turns of
-            []          -> 0
-            (Board b:_) -> length b
+    height = case turns of
+              []          -> 0
+              (Board b:_) -> length b
