f17240fa584d08cb54180ed2093b43c98f7fb033
Andrew Bruce <me@andrewbruce.net>
2017-03-18 19:07:55 +0000

    layout

--- src/Board.hs
+++ src/Board.hs
@@ -1,6 +1,6 @@
 module Board
-    ( generateBoard
-    ) where
+  ( generateBoard
+  ) where
 
 import           System.Random
 
@@ -8,10 +8,10 @@ import           Model
 
 generateBoard :: StdGen -> Int -> Int -> Board
 generateBoard g width height
-  | internal width >= 1 && internal height >= 1 =
-      generateBoardWithContent g width height
-  | otherwise =
-      Board $ replicate height $ replicate width Wall
+  | internal width >= 1 && internal height >= 1
+  = generateBoardWithContent g width height
+  | otherwise
+  = Board $ replicate height $ replicate width Wall
 
 generateBoardWithContent :: StdGen -> Int -> Int -> Board
 generateBoardWithContent g width height =
@@ -25,19 +25,17 @@ generateBoardWithContent g width height =
     rows = map row [1 .. internal height]
 
 choosePlayerPoint :: StdGen -> Int -> Int -> Point
-choosePlayerPoint g w h = (playerX, playerY)
-  where
-    (playerX, g') = randomR (1, w) g
-    (playerY, _) = randomR (1, h) g'
+choosePlayerPoint g w h = (playerX, playerY) where
+  (playerX, g') = randomR (1, w) g
+  (playerY, _g) = randomR (1, h) g'
 
 rowItems :: Point -> Int -> [Tile]
-rowItems playerPoint y =
-  map (\x -> newTile playerPoint (x, y)) [1..]
-  where
-    newTile point candidatePoint =
-      if point == candidatePoint
-        then Grass (Just newPlayer)
-        else Grass Nothing
+rowItems playerPoint y = map newTile [1..] where
+  newTile x
+    | (x, y) == playerPoint
+    = Grass (Just newPlayer)
+    | otherwise
+    = Grass Nothing
 
 horzWall :: Int -> [Tile]
 horzWall width = replicate width Wall
