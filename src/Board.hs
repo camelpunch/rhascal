c28b7b153c2a05c28ec3c102f143bccda7eb5bce
Andrew Bruce <me@andrewbruce.net>
2017-04-05 00:18:40 +0100

    refactor

--- src/Board.hs
+++ src/Board.hs
@@ -15,11 +15,52 @@ import           System.Random
 import           Model
 
 generateBoard :: StdGen -> Int -> Int -> Board
-generateBoard g w h
-  | internal w >= 1 && internal h >= 1
-  = generateBoardWithContent g w h
-  | otherwise
-  = Board $ replicate h $ replicate w Wall
+generateBoard g w h = Board generateRows where
+
+  generateRows
+    | internal w >= 1 && internal h >= 1
+    = [horzWall] ++ rows ++ [horzWall]
+    | otherwise
+    = replicate h horzWall
+
+  horzWall = replicate w Wall
+
+  rows = map row [1 .. internal h]
+
+  row y = [Wall] ++ internalRow y ++ [Wall]
+
+  internalRow y =
+    take (internal w) $
+         rowItems chosenPlayerCoords y
+
+  rowItems playerCoords y = map newTile [1..] where
+    newTile x
+      | (x, y) == playerCoords
+      = Grass (Just newPlayer)
+      | otherwise
+      = Grass Nothing
+
+  chosenPlayerCoords = (x, y) where
+    (x, g') = randomR (1, internal w) g
+    (y, _g) = randomR (1, internal h) g'
+
+  newPlayer = Character { piece = Piece '@'
+                        , hitPoints = 1
+                        , armourClass = 1}
+
+  internal = subtract 2
+
+count :: (Tile -> Bool) -> Board -> Int
+count f = length . filter f . tiles
+
+width :: Board -> Int
+width (Board rows) = maybe 0 (length . fst) $ uncons rows
+
+height :: Board -> Int
+height (Board rows) = length rows
+
+tiles :: Board -> [Tile]
+tiles (Board rows) = concat rows
 
 left :: (Character -> Bool) -> Board -> Board
 left f (Board rows) = Board $ map (moveBackOnAxis f) rows
@@ -61,52 +102,3 @@ moveBackOnAxis f xs =
 
 moveForwardOnAxis :: (Character -> Bool) -> [Tile] -> [Tile]
 moveForwardOnAxis f = reverse . moveBackOnAxis f . reverse
-
-generateBoardWithContent :: StdGen -> Int -> Int -> Board
-generateBoardWithContent g w h =
-  Board $ [horzWall w] ++ rows ++ [horzWall w]
-  where
-    row y = [Wall] ++ internalRow y ++ [Wall]
-    internalRow y =
-      take (internal w) $
-           rowItems (choosePlayerPoint g (internal w) (internal h))
-                    y
-    rows = map row [1 .. internal h]
-
-choosePlayerPoint :: StdGen -> Int -> Int -> Point
-choosePlayerPoint g w h = (playerX, playerY) where
-  (playerX, g') = randomR (1, w) g
-  (playerY, _g) = randomR (1, h) g'
-
-rowItems :: Point -> Int -> [Tile]
-rowItems playerPoint y = map newTile [1..] where
-  newTile x
-    | (x, y) == playerPoint
-    = Grass (Just newPlayer)
-    | otherwise
-    = Grass Nothing
-
-count :: (Tile -> Bool) -> Board -> Int
-count f = length . filter f . tiles
-
-width :: Board -> Int
-width (Board rows) = case uncons rows of
-  Nothing     -> 0
-  Just (h, _) -> length h
-
-height :: Board -> Int
-height (Board rows) = length rows
-
-tiles :: Board -> [Tile]
-tiles (Board rows) = concat rows
-
-horzWall :: Int -> [Tile]
-horzWall w = replicate w Wall
-
-newPlayer :: Character
-newPlayer = Character { piece = Piece '@'
-                      , hitPoints = 1
-                      , armourClass = 1}
-
-internal :: Int -> Int
-internal n = n - 2
